3b20bc355c87fe151b5194a06bc0d91f64019305
ahsankhodami <ahsan.khodami@gmail.com>
2025-05-12 19:45:15 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="scia",
-    use_scm_version={
-        'write_to': 'scia/_version.py',
-        'write_to_template': '__version__ = "{version}"'
-    },
-    setup_requires=["setuptools-scm"],
+    name="scia",  # must match [project.name] in pyproject.toml
+    packages=find_packages(),
+    include_package_data=True,
     author="Mohammad Ahsan Khodami",
-    author_email="ahsan.khodami@gmail.com", 
+    author_email="ahsan.khodami@gmail.com",
     description="A Comprehensive most updated Python package for Single Case Design Analysis",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Ahsankhodami/scia",
-    packages=find_packages(),
-    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -27,8 +22,7 @@ setup(
         "matplotlib",
         "scipy",
         "openpyxl",
-        "scikit-learn",  # Fixed the extra space
-        # "scipy",  # Removed duplicate
+        "scikit-learn",
         "statsmodels"
     ],
 )
